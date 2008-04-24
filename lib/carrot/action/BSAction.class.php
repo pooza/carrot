1f0f7da4d139a1630bca7da03340ed6d0ee194a5
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2008-04-24 14:11:43 +0000

    Actionクラスを廃止
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@254 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- lib/carrot/action/BSAction.class.php
+++ lib/carrot/action/BSAction.class.php
@@ -5,15 +5,53 @@
  */
 
 /**
- * Actionのラッパー
+ * アクション
  *
  * @author 小石達也 <tkoishi@b-shock.co.jp>
  * @copyright (c)b-shock. co., ltd.
  * @version $Id$
  * @abstract
  */
-abstract class BSAction extends Action {
+abstract class BSAction {
 	protected $recordClassName;
+
+	abstract public function execute ();
+
+	public function initialize () {
+		return true;
+	}
+
+	public function __get ($name) {
+		switch ($name) {
+			case 'controller':
+				return BSController::getInstance();
+			case 'request':
+				return BSRequest::getInstance();
+			case 'user':
+				return BSUser::getInstance();
+			case 'database':
+				return BSDatabase::getInstance();
+		}
+	}
+
+	public function getDefaultView () {
+		return BSView::SUCCESS;
+	}
+
+	public function handleError () {
+		return BSView::ERROR;
+	}
+
+	public function registerValidators ($validators) {
+	}
+
+	public function validate () {
+		return true;
+	}
+
+	public function isSecure () {
+		return false;
+	}
 
 	/**
 	 * レコードクラス名を返す
@@ -33,21 +71,12 @@ abstract class BSAction extends Action {
 		return $this->recordClassName;
 	}
 
-	public function __get ($name) {
-		switch ($name) {
-			case 'controller':
-				return BSController::getInstance();
-			case 'request':
-				return BSRequest::getInstance();
-			case 'user':
-				return BSUser::getInstance();
-			case 'database':
-				return BSDatabase::getInstance();
-		}
+	public function getCredential () {
+		return null;
 	}
 
-	public function getDefaultView () {
-		return BSView::SUCCESS;
+	public function getRequestMethods () {
+		return BSRequest::GET | BSRequest::POST;
 	}
 }
 
