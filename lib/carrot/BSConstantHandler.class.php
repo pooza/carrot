5c749eb08b47e9762b9193414d0e6a8c37134998
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2008-06-07 05:52:59 +0000

    ConstantHandlerの基本部分を実装。（refs #35）
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@394 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- lib/carrot/BSConstantHandler.class.php
+++ lib/carrot/BSConstantHandler.class.php
@@ -10,7 +10,70 @@
  * @copyright (c)b-shock. co., ltd.
  * @version $Id$
  */
-class BSConstantHandler implements BSDictionary {
+class BSConstantHandler extends BSParameterHolder implements BSDictionary {
+
+	/**
+	 * パラメータを返す
+	 *
+	 * @access public
+	 * @param string $name パラメータ名
+	 * @return mixed パラメータ
+	 */
+	public function getParameter ($name) {
+		$name = strtoupper($name);
+		if ($this->hasParameter($name)) {
+			return constant($name);
+		}
+	}
+
+	/**
+	 * パラメータを設定する
+	 *
+	 * @access public
+	 * @param string $name パラメータ名
+	 * @param mixed $value 値
+	 */
+	public function setParameter ($name, $value) {
+		$name = strtoupper($name);
+		if ($this->hasParameter($name)) {
+			throw new BSException('定数 "%s" は定義済みです。', $name);
+		} else {
+			define($name, $value);
+		}
+	}
+
+	/**
+	 * 全てのパラメータを返す
+	 *
+	 * @access public
+	 * @return mixed[] 全てのパラメータ
+	 */
+	public function getParameters () {
+		$constants = get_defined_constants(true);
+		return new BSArray($constants['user']);
+	}
+
+	/**
+	 * パラメータが存在するか？
+	 *
+	 * @access public
+	 * @param string $name パラメータ名
+	 * @return boolean 存在すればTrue
+	 */
+	public function hasParameter ($name) {
+		$name = strtoupper($name);
+		return defined($name);
+	}
+
+	/**
+	 * パラメータを削除する
+	 *
+	 * @access public
+	 * @param string $name パラメータ名
+	 */
+	public function removeParameter ($name) {
+		throw new BSException('定数は削除できません。');
+	}
 
 	/**
 	 * 名前を返す
@@ -31,13 +94,10 @@ class BSConstantHandler implements BSDictionary {
 	 * @return string 翻訳された文字列
 	 */
 	public function translate ($label, $language) {
-		$labels = array(
-			strtoupper($label),
-			strtoupper($label . '_' . $language),
-		);
+		$labels = array($label, $label . '_' . $language);
 		foreach ($labels as $label) {
-			if (defined($label)) {
-				return constant($label);
+			if ($value = $this->getParameter($label)) {
+				return $value;
 			}
 		}
 	}
