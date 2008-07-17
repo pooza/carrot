0709238575b4516b17a51ed2851ce2aabe51b904
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2008-07-17 09:06:06 +0000

    fixes #93
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@464 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- lib/carrot/action/BSAction.class.php
+++ lib/carrot/action/BSAction.class.php
@@ -18,10 +18,23 @@ abstract class BSAction implements BSHTTPRedirector {
 	private $views;
 	protected $recordClassName;
 
+	/**
+	 * コンストラクタ
+	 *
+	 * @access public
+	 * @params BSModule $module 呼び出し元モジュール
+	 */
 	public function __construct (BSModule $module) {
 		$this->module = $module;
 	}
 
+	/**
+	 * プロパティ取得のオーバライド
+	 *
+	 * @access public
+	 * @param string $name プロパティ名
+	 * @return mixed 各種オブジェクト
+	 */
 	public function __get ($name) {
 		switch ($name) {
 			case 'controller':
@@ -42,29 +55,75 @@ abstract class BSAction implements BSHTTPRedirector {
 		}
 	}
 
+	/**
+	 * 実行
+	 *
+	 * getRequestMethodsで指定されたメソッドでリクエストされた場合に実行される。
+	 *
+	 * @access public
+	 * @return string ビュー名
+	 * @abstract
+	 */
 	abstract public function execute ();
 
+	/**
+	 * 初期化
+	 *
+	 * Falseを返すと、例外が発生。
+	 *
+	 * @access public
+	 * @return boolean 正常終了ならTrue
+	 */
 	public function initialize () {
 		return true;
 	}
 
+	/**
+	 * デフォルト時ビュー
+	 *
+	 * getRequestMethodsに含まれていないメソッドから呼び出されたとき、
+	 * executeではなくこちらが実行される。
+	 *
+	 * @access public
+	 * @return string ビュー名
+	 */
 	public function getDefaultView () {
 		return BSView::SUCCESS;
 	}
 
+	/**
+	 * エラー時処理
+	 *
+	 * バリデート結果が妥当でなかったときに実行される。
+	 *
+	 * @access public
+	 * @return string ビュー名
+	 */
 	public function handleError () {
 		return BSView::ERROR;
 	}
 
+	/**
+	 * バリデータ登録
+	 *
+	 * 動的に登録しなければならないバリデータを、ここで登録。
+	 *
+	 * @access public
+	 */
 	public function registerValidators () {
 	}
 
+	/**
+	 * 論理バリデーション
+	 *
+	 * 定義ファイルで吸収できない、複雑なバリデーションをここに記述。
+	 * registerValidatorsで実現できないか、まずは検討すべき。
+	 *
+	 * @access public
+	 * @return boolean 妥当な入力ならTrue
+	 */
 	public function validate () {
 		return true;
-	}
-
-	public function isSecure () {
-		return false;
 	}
 
 	/**
@@ -170,10 +229,24 @@ abstract class BSAction implements BSHTTPRedirector {
 		return $this->recordClassName;
 	}
 
+	/**
+	 * 必要なクレデンシャルを返す
+	 *
+	 * @access public
+	 * @return string 必要なクレデンシャル
+	 */
 	public function getCredential () {
 		return null;
 	}
 
+	/**
+	 * 正規なリクエストとして扱うメソッド
+	 *
+	 * ここに指定したリクエストではexecuteが、それ以外ではgetDefaultViewが実行される
+	 *
+	 * @access public
+	 * @return integer メソッドのビット列
+	 */
 	public function getRequestMethods () {
 		return BSRequest::GET | BSRequest::POST | BSRequest::PUT | BSRequest::DELETE;
 	}
