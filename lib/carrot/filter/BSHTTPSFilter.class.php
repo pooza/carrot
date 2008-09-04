b5a5c7cfedb680cdba70b2c5d0f58246d2a5f6e7
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2008-09-04 11:44:15 +0000

    fixes #7
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@545 4d49281a-7d3a-dc11-b19b-000e0cb57199

---
+++ lib/carrot/filter/BSHTTPSFilter.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package org.carrot-framework
+ * @subpackage filter
+ */
+
+/**
+ * HTTPSによるGETを強制するフィルタ
+ *
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ * @copyright (c)b-shock. co., ltd.
+ * @version $Id$
+ */
+class BSHTTPSFilter extends BSFilter {
+	public function execute (BSFilterChain $filters) {
+		if (!$this->controller->isDebugMode()
+			&& !$this->controller->isCLI()
+			&& !$this->controller->isSSL()
+			&& ($this->request->getMethod() == BSRequest::GET)) {
+
+			$url = new BSURL($this->controller->getConstant('ROOT_URL_HTTPS'));
+			$url->setAttribute('path', $this->controller->getEnvironment('REQUEST_URI'));
+			$this->controller->redirect($url);
+			exit;
+		}
+		$filters->execute();
+	}
+}
+
+/* vim:set tabstop=4 ai: */
+?>
