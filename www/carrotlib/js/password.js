5e6014b105c4971272c83673f6900d5f90301775
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2008-12-16 02:13:14 +0000

    fixes #291
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@748 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- www/carrotlib/js/password.js
+++ www/carrotlib/js/password.js
@@ -1,34 +1,19 @@
 /**
  * パスワード生成 JavaScript
  *
+ * 要、rand.js
+ *
  * @package org.carrot-framework
  * @author 小石達也 <tkoishi@b-shock.co.jp>
  * @version $Id$
  */
 
 function getRandomPassword (length) {
-  src = 'ABCDEFGHJKLMNPRSTUVWXYZ2345678';
-  password = '';
-  for (i = 0 ; i < length ; i ++) {
-    index = rand(src.length) - 1;
+  var src = 'ABCDEFGHJKLMNPRSTUVWXYZ2345678';
+  var password = '';
+  for (var i = 0 ; i < length ; i ++) {
+    var index = rand(src.length) - 1;
     password += src.slice(index, index + 1);
   }
   return password;
 }
-
-// The Central Randomizer 1.3 (C) 1997 by Paul Houle (houle@msc.cornell.edu)
-// See: http://www.msc.cornell.edu/~houle/javascript/randomizer.html
-
-rnd.today=new Date();
-rnd.seed=rnd.today.getTime();
-
-function rnd() {
-rnd.seed = (rnd.seed*9301+49297) % 233280;
-return rnd.seed/(233280.0);
-};
-
-function rand(number) {
-return Math.ceil(rnd()*number);
-};
-
-// end central randomizer. -->
