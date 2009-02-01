d92cb7db0cca68902cb8c36b1b5be10bbf818bef
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2009-02-01 10:04:40 +0000

    fixes #329
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@826 4d49281a-7d3a-dc11-b19b-000e0cb57199

---
+++ lib/carrot/action/BSPaginateTableAction.class.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package org.carrot-framework
+ * @subpackage action
+ */
+
+/**
+ * ページあり一覧画面用 アクションひな形
+ *
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ * @version $Id$
+ * @abstract
+ */
+abstract class BSPaginateTableAction extends BSTableAction {
+
+	/**
+	 * ページ番号を返す
+	 *
+	 * @access protected
+	 * @return integer ページ番号
+	 */
+	protected function getPage () {
+		if (!$this->page) {
+			if (!$page = $this->request['page']) {
+				$page = 1;
+			}
+			$this->getTable()->setPage($page);
+			$this->page = $this->getTable()->getPage();
+		}
+		return $this->page;
+	}
+
+	/**
+	 * ページサイズを返す
+	 *
+	 * @access public
+	 * @return integer ページサイズ
+	 */
+	protected function getPageSize () {
+		return 50;
+	}
+
+	/**
+	 * テーブルを返す
+	 *
+	 * @access public
+	 * @return BSTableHandler テーブル
+	 */
+	public function getTable () {
+		if (!$this->table) {
+			$this->table = parent::getTable();
+			$this->table->setPageSize($this->getPageSize());
+			$this->table->setPage($this->getPage());
+		}
+		return $this->table;
+	}
+
+	public function initialize () {
+		parent::initialize();
+		$this->request->setAttribute('page', $this->getPage());
+		$this->request->setAttribute('lastpage', $this->getTable()->getLastPage());
+		return true;
+	}
+}
+
+/* vim:set tabstop=4: */
