76634a6d7682015fd0a3f0d9fd6ba2a9e883aa85
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2009-02-16 01:17:27 +0000

    refs #343
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@860 4d49281a-7d3a-dc11-b19b-000e0cb57199

---
+++ lib/carrot/session/BSSessionException.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package org.carrot-framework
+ * @subpackage session
+ */
+
+/**
+ * セッション例外
+ *
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ * @version $Id$
+ */
+class BSSessionException extends BSException {
+}
+
+/* vim:set tabstop=4: */
