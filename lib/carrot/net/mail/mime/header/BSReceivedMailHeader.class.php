40400a154d7ee23845cbaaf4f652667080afb44c
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2009-02-24 14:07:40 +0000

    fixes #391
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@917 4d49281a-7d3a-dc11-b19b-000e0cb57199

---
+++ lib/carrot/net/mail/mime/header/BSReceivedMailHeader.class.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @package org.carrot-framework
+ * @subpackage net.mail.mime.header
+ */
+
+/**
+ * Receivedメールヘッダ
+ *
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ * @version $Id$
+ */
+class BSReceivedMailHeader extends BSMailHeader {
+	private $servers = array();
+
+	/**
+	 * 実体を返す
+	 *
+	 * @access public
+	 * @return mixed 実体
+	 */
+	public function getEntity () {
+		return new BSArray($this->servers);
+	}
+
+	/**
+	 * 内容を返す
+	 *
+	 * @access public
+	 * @return string 内容
+	 */
+	public function getContents () {
+		return $this->getEntity()->join("\n");
+	}
+
+	/**
+	 * 内容を設定
+	 *
+	 * @access public
+	 * @param mixed $contents 内容
+	 */
+	public function setContents ($contents) {
+		parent::setContents($contents);
+		$this->servers[] = $this->contents;
+	}
+
+	/**
+	 * 内容を追加
+	 *
+	 * @access public
+	 * @param string $contents 内容
+	 */
+	public function appendContents ($contents) {
+		parent::appendContents($contents);
+		$this->servers[count($this->servers) - 1] = $this->contents;
+	}
+
+	/**
+	 * 複数行を許容するか？
+	 *
+	 * @access public
+	 * @return boolean 許容ならばTrue
+	 */
+	public function isMultiple () {
+		return true;
+	}
+
+	/**
+	 * 可視か？
+	 *
+	 * @access public
+	 * @return boolean 可視ならばTrue
+	 */
+	public function isVisible () {
+		return false;
+	}
+}
+
+/* vim:set tabstop=4: */
