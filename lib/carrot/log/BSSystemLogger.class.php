c200c4f6ed918a5a58937c24aca6c1a8a399a3ca
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2009-02-26 04:21:34 +0000

    fixes #403
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@929 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- lib/carrot/log/BSSystemLogger.class.php
+++ lib/carrot/log/BSSystemLogger.class.php
@@ -26,7 +26,7 @@ class BSSystemLogger extends BSLogger {
 	 * @return string 利用可能ならTrue
 	 */
 	public function initialize () {
-		openlog('carrot', LOG_PID | LOG_PERROR, LOG_LOCAL6);
+		return openlog('carrot', LOG_PID | LOG_PERROR, LOG_LOCAL6);
 	}
 
 	/**
