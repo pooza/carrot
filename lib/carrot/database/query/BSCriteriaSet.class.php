7ce22d3187bde7e60eaf32971b9d55c4756d1222
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2009-03-27 07:20:09 +0000

    fixes #525
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@1020 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- lib/carrot/database/query/BSCriteriaSet.class.php
+++ lib/carrot/database/query/BSCriteriaSet.class.php
@@ -7,8 +7,6 @@
 /**
  * 抽出条件の集合
  *
- * 未実装
- *
  * @author 小石達也 <tkoishi@b-shock.co.jp>
  * @version $Id$
  */
@@ -16,14 +14,32 @@ class BSCriteriaSet extends BSArray {
 	private $glue = 'AND';
 	private $db;
 
+	/**
+	 * 接続子を返す
+	 *
+	 * @access public
+	 * @return string 接続子
+	 */
 	public function getGlue () {
 		return $this->glue;
 	}
 
+	/**
+	 * 接続子を設定
+	 *
+	 * @access public
+	 * @param string $glue 接続子
+	 */
 	public function setGlue ($glue) {
 		$this->glue = strtoupper($glue);
 	}
 
+	/**
+	 * 対象データベースを返す
+	 *
+	 * @access public
+	 * @return BSDatabase 対象データベース
+	 */
 	public function getDatabase () {
 		if (!$this->db) {
 			$this->db = BSDatabase::getInstance();
@@ -31,26 +47,68 @@ class BSCriteriaSet extends BSArray {
 		return $this->db;
 	}
 
+	/**
+	 * 対象データベースを設定
+	 *
+	 * @access public
+	 * @param BSDatabase $db 対象データベース
+	 */
 	public function setDatabase (BSDatabase $db) {
 		$this->db = $db;
 	}
 
+	/**
+	 * 条件を登録
+	 *
+	 * @access public
+	 * @param string $key フィールド名
+	 * @param mixed $value 値又はその配列
+	 * @param string $operator 演算子
+	 */
 	public function register ($key, $value, $operator = '=') {
-		if (strtoupper($operator) == 'IN') {
+		$key = strtolower($key);
+		$operator = strtoupper($operator);
+
+		if ($operator == 'IN') {
+			if (BSArray::isArray($value)) {
+				$values = new BSArray($value);
+				if ($values->count()) {
+					$this[] = $key . ' IN (' . $this->quote($values)->join(',') . ')';
+				}
+				return;
+			} else {
+				$operator = '=';
+			}
+		}
+		$this[] = $key . $operator . $this->quote($value);
+	}
+
+	/**
+	 * 値をクォート
+	 *
+	 * @access public
+	 * @param mixed $value 値又はその配列
+	 * @param string $operator 演算子
+	 * @return mixed クォートされた値
+	 */
+	public function quote ($value) {
+		if (BSArray::isArray($value)) {
 			$ids = new BSArray;
 			foreach (new BSArray($value) as $item) {
 				$ids[] = $this->quote($item);
 			}
-			$this[] = strtolower($key) . ' IN (' . $ids->join(',') . ')';
+			return $ids;
 		} else {
-			$this[] = strtolower($key) . strtoupper($operator) . $this->quote($value);
+			return $this->getDatabase()->quote($value);
 		}
 	}
 
-	public function quote ($value) {
-		return $this->getDatabase()->quote($value);
-	}
-
+	/**
+	 * 内容を返す
+	 *
+	 * @access public
+	 * @return string 内容
+	 */
 	public function getContents () {
 		$contents = new BSArray;
 		foreach ($this as $criteria) {
