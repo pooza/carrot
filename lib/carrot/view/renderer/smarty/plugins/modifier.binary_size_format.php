5df801384d999ad4eaca6d6d339ffa76c94474b3
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2009-04-28 12:45:48 +0000

    BSSmarty binary_size_format修飾子
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@1118 4d49281a-7d3a-dc11-b19b-000e0cb57199

---
+++ lib/carrot/view/renderer/smarty/plugins/modifier.binary_size_format.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package org.carrot-framework
+ * @subpackage view.renderer.smarty.plugins
+ */
+
+/**
+ * バイナリサイズ修飾子
+ *
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ * @version $Id$
+ */
+function smarty_modifier_binary_size_format ($value) {
+	if (is_array($value)) {
+		return $value;
+	} else if ($value instanceof BSParameterHolder) {
+		return $value->getParameters();
+	} else if (!BSString::isBlank($value)) {
+		return BSNumeric::getBinarySize($value);
+	}
+}
+
+/* vim:set tabstop=4: */
