77ccf7d2670982dabab62ac682970eea30b10b20
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2009-05-15 07:24:52 +0000

    fixes #750
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@1192 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- webapp/modules/Default/views/DeniedUserAgentErrorView.class.php
+++ webapp/modules/Default/views/DeniedUserAgentErrorView.class.php
@@ -10,6 +10,7 @@
 class DeniedUserAgentErrorView extends BSSmartyView {
 	public function execute () {
 		$this->setTemplate('DeniedUserAgent');
+		$this->setHeader('Status', '403 Forbidden');
 	}
 }
 
