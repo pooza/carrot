6fd6c7f2dfa3683a609100108515728b8efdf190
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2009-06-24 09:40:43 +0000

    fixes #877
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@1294 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- lib/carrot/csv/BSCSVData.class.php
+++ lib/carrot/csv/BSCSVData.class.php
@@ -36,6 +36,7 @@ class BSCSVData implements BSTextRenderer, IteratorAggregate {
 	 * @param BSArray $lines 
 	 */
 	public function setLines (BSArray $lines) {
+		$this->records = new BSArray;
 		foreach ($lines as $line) {
 			if (isset($record) && $record) {
 				$record .= "\n" . $line;
