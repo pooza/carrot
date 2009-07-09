7f00c86f97fe1e57bf8ae2f245cfc804dc2e1cbd
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2009-07-09 15:35:46 +0000

    fixes #894
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@1309 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- www/carrotlib/js/elevator.js
+++ www/carrotlib/js/elevator.js
@@ -1,41 +1,29 @@
 /**
  * エレベータ処理
  *
- * 要 prototype.js
- *
- * example:
- * var elevator = new Elevator('div_id', 200, 20, 10);
- * new PeriodicalExecuter(function() {elevator.move()}, 0.1);
- *
  * @package org.carrot-framework
  * @author 小石達也 <tkoishi@b-shock.co.jp>
  * @version $Id$
  */
 
 function Elevator (id, x, yMin, yMargin) {
-  this.element = $(id);
-  this.x = x;
-  this.yMin = yMin;
-  this.yMargin = yMargin;
+  var element = $(id);
+  new PeriodicalExecuter(move, 0.1);
 
-  this.move = function () {
+  function move () {
     if (navigator.userAgent.match(/MSIE/)){
       var y = (document.body.scrollTop || document.documentElement.scrollTop);
     } else  {
       var y = self.pageYOffset;
     }
-    if (y < this.yMin) {
-      y = this.yMin;
+    if (y < yMin) {
+      y = yMin;
     } else {
-      y = y + this.yMargin;
+      y = y + yMargin;
     }
 
-    this.element.style.position = 'absolute';
-    this.element.style.left = x + 'px';
-    this.element.style.top = y + 'px';
-  }
-
-  this.getElement = function () {
-    return this.element;
+    element.style.position = 'absolute';
+    element.style.left = x + 'px';
+    element.style.top = y + 'px';
   }
 }
