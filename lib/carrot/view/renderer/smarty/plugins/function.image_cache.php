b0a49da59db644c298ae027b647e42878a612030
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2009-08-03 05:17:20 +0000

    refs #984
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@1359 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- lib/carrot/view/renderer/smarty/plugins/function.image_cache.php
+++ lib/carrot/view/renderer/smarty/plugins/function.image_cache.php
@@ -16,20 +16,24 @@ function smarty_function_image_cache ($params, &$smarty) {
 	$caches = BSImageCacheHandler::getInstance();
 	$params = new BSArray($params);
 
-	if ($container = $caches->getContainer($params)) {
-		$flags = $caches->convertFlags($params['flags']);
-		$info = $caches->getImageInfo($container, $params['size'], $params['pixel'], $flags);
-
-		if (BSString::toLower($params['mode']) == 'size') {
-			return $info['pixel_size'];
-		}
-
-		$element = $caches->getImageElement($info);
-		if (($class = $params['style_class']) && !$smarty->getUserAgent()->isMobile()) {
-			$element->setAttribute('class', $class);
-		}
-		return $element->getContents();
+	if (!$container = $caches->getContainer($params)) {
+		return null;
 	}
+
+	$flags = $caches->convertFlags($params['flags']);
+	if (!$info = $caches->getImageInfo($container, $params['size'], $params['pixel'], $flags)) {
+		return null;
+	}
+
+	if (BSString::toLower($params['mode']) == 'size') {
+		return $info['pixel_size'];
+	}
+
+	$element = $caches->getImageElement($info);
+	if (($class = $params['style_class']) && !$smarty->getUserAgent()->isMobile()) {
+		$element->setAttribute('class', $class);
+	}
+	return $element->getContents();
 }
 
 /* vim:set tabstop=4: */
