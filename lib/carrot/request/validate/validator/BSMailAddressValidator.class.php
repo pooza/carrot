c4b901f01078825841b4e4126035b3b673676cea
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2009-09-19 04:05:12 +0000

    fixes #1149
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@1503 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- lib/carrot/request/validate/validator/BSMailAddressValidator.class.php
+++ lib/carrot/request/validate/validator/BSMailAddressValidator.class.php
@@ -13,55 +13,16 @@
 class BSMailAddressValidator extends BSValidator {
 
 	/**
-	 * 対象テーブルを返す
-	 *
-	 * @access private
-	 * @return BSTableHandler 対象テーブル
-	 */
-	private function getTable () {
-		if (!$class = $this['class']) {
-			$class = $this['table'];
-		}
-		return BSTableHandler::getInstance($class);
-	}
-
-	/**
-	 * 登録済みのアドレスか
-	 *
-	 * @access private
-	 * @param BSMailAddress $email メールアドレス
-	 * @return boolean 登録済みならTrue
-	 */
-	private function isRegisteredAddress (BSMailAddress $email) {
-		$values = array($this['field'] => $email->getContents());
-		if ($record = $this->getTable()->getRecord($values)) {
-			if ($id = $this->controller->getModule()->getRecordID()) {
-				if ($id != $record->getID()) {
-					return true;
-				}
-			} else {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	/**
 	 * 初期化
 	 *
 	 * @access public
 	 * @param string[] $parameters パラメータ配列
 	 */
 	public function initialize ($parameters = array()) {
-		$this['unique'] = false;
-		$this['unique_error'] = '重複します。';
 		$this['domain'] = false;
 		$this['domain_error'] = '正しいドメインではない様です。';
 		$this['mobile_allowed'] = true;
 		$this['mobile_allowed_error'] = '携帯電話用のアドレスは使用できません。';
-		$this['table'] = 'account';
-		$this['class'] = null;
-		$this['field'] = 'email';
 		$this['invalid_error'] = '正しいメールアドレスではありません。';
 		return parent::initialize($parameters);
 	}
@@ -86,11 +47,6 @@ class BSMailAddressValidator extends BSValidator {
 
 		if (!$this['mobile_allowed'] && $email->isMobile()) {
 			$this->error = $this['mobile_allowed_error'];
-			return false;
-		}
-
-		if ($this['unique'] && $this->isRegisteredAddress($email)) {
-			$this->error = $this['unique_error'];
 			return false;
 		}
 
