edefd541258d1c77cf0b6f20e99f99a93cc046f7
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2009-10-19 13:41:56 +0000

    refs #1234
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@1571 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- lib/carrot/media/flash/BSFlashFile.class.php
+++ lib/carrot/media/flash/BSFlashFile.class.php
@@ -10,49 +10,24 @@
  * @author 小石達也 <tkoishi@b-shock.co.jp>
  * @version $Id$
  */
-class BSFlashFile extends BSFile implements ArrayAccess {
-	private $attributes;
+class BSFlashFile extends BSMediaFile {
 
 	/**
-	 * 属性を返す
+	 * ファイルを解析
 	 *
-	 * @access public
-	 * @param string $name 属性名
-	 * @return mixed 属性
+	 * @access protected
 	 */
-	public function getAttribute ($name) {
-		return $this->getAttributes()->getParameter($name);
-	}
-
-	/**
-	 * 全ての属性を返す
-	 *
-	 * @access public
-	 * @return BSArray 全ての属性
-	 */
-	public function getAttributes () {
-		if (!$this->attributes) {
-			$this->attributes = new BSArray;
-			$info = getimagesize($this->getPath());
-			if (!$info || ($info['mime'] != $this->getType())) {
-				throw new BSFlashException($this . 'はFlashムービーではありません。');
-			}
-			$this->attributes['path'] = $this->getPath();
-			$this->attributes['width'] = $info[0];
-			$this->attributes['height'] = $info[1];
-			$this->attributes['pixel_size'] = $this['width'] . '×' . $this['height'];
+	protected function analize () {
+		$info = getimagesize($this->getPath());
+		if (!$info || ($info['mime'] != BSMIMEType::getType('swf'))) {
+			throw new BSFlashException($this . 'はFlashムービーではありません。');
 		}
-		return $this->attributes;
-	}
-
-	/**
-	 * メディアタイプを返す
-	 *
-	 * @access public
-	 * @return string メディアタイプ
-	 */
-	public function getType () {
-		return BSMIMEType::getType('swf');
+		$this->attributes['path'] = $this->getPath();
+		$this->attributes['width'] = $info[0];
+		$this->attributes['type'] = $info['mime'];
+		$this->attributes['height'] = $info[1];
+		$this->attributes['height_full'] = $info[1];
+		$this->attributes['pixel_size'] = $this['width'] . '×' . $this['height'];
 	}
 
 	/**
@@ -63,77 +38,26 @@ class BSFlashFile extends BSFile implements ArrayAccess {
 	 * @return BSXMLElement 要素
 	 */
 	public function getImageElement (BSParameterHolder $params) {
+		$constants = BSConstantHandler::getInstance();
 		foreach (array('href_prefix', 'player_ver', 'installer_href') as $key) {
 			if (BSString::isBlank($params[$key])) {
-				$params[$key] = BSController::getInstance()->getConstant('flash_' . $key);
+				$params[$key] = $constants['flash_' . $key];
 			}
 		}
-
-		$style = $this->getPixelSizeCSSSelector($params);
-		$root = new BSXMLElement('div');
-		if (!BSString::isBlank($params['style_class'])) {
-			$root->setAttribute('class', $params['style_class']);
-		}
-		if ($params['mode'] == 'noscript') {
-			$root->setAttribute('style', $style->getContents());
-			$root->addElement($this->getObjectElement($params));
-		} else {
-			if (BSString::isBlank($params['container_id'])) {
-				$params['container_id'] = $this->getContainerID();
-				$container = $root->createElement('div');
-				$container->setAttribute('id', $params['container_id']);
-			}
-			$root->addElement($this->getScriptElement($params));
-		}
-		return $root;
-	}
-
-	/**
-	 * スタイル属性を返す
-	 *
-	 * @access private
-	 * @param BSParameterHolder $params パラメータ配列
-	 * @return BSCSSSelector スタイル属性
-	 */
-	private function getPixelSizeCSSSelector (BSParameterHolder $params) {
-		$style = new BSCSSSelector;
-		if ($params['width']) {
-			$style['width'] = $params['width'] . 'px';
-		} else {
-			$style['width'] = $this['width'] . 'px';
-		}
-		if ($params['height']) {
-			$style['height'] = $params['height'] . 'px';
-		} else {
-			$style['height'] = $this['height'] . 'px';
-		}
-		return $style;
-	}
-
-	/**
-	 * div要素のIDを返す
-	 *
-	 * @access private
-	 * @return string div要素のID
-	 */
-	private function getContainerID () {
-		return 'swf_' . $this->getID();
+		return parent::getImageElement($params);
 	}
 
 	/**
 	 * script要素を返す
 	 *
-	 * @access private
+	 * @access protected
 	 * @param BSParameterHolder $params パラメータ配列
 	 * @return BSXMLElement 要素
 	 */
-	private function getScriptElement (BSParameterHolder $params) {
-		$url = BSURL::getInstance();
-		$url['path'] = $params['href_prefix'] . $this->getName() . $params['href_suffix'];
-
+	protected function getScriptElement (BSParameterHolder $params) {
 		$element = BSJavaScriptUtility::getScriptElement();
 		$body = new BSStringFormat('swfobject.embedSWF(%s,%s,%d,%d,%s,%s,%s,%s);');
-		$body[] = BSJavaScriptUtility::quote($url->getContents());
+		$body[] = BSJavaScriptUtility::quote($this->getMediaURL($params)->getContents());
 		$body[] = BSJavaScriptUtility::quote($params['container_id']);
 		$body[] = $this['width'];
 		$body[] = $this['height'];
@@ -148,80 +72,14 @@ class BSFlashFile extends BSFile implements ArrayAccess {
 	/**
 	 * object要素を返す
 	 *
-	 * @access private
+	 * @access protected
 	 * @param BSParameterHolder $params パラメータ配列
 	 * @return BSXMLElement 要素
 	 */
-	private function getObjectElement (BSParameterHolder $params) {
-		$url = BSURL::getInstance();
-		$url['path'] = $params['href_prefix'] . $this->getName() . $params['href_suffix'];
-		if (BSUser::getInstance()->isAdministrator()) {
-			$url->setParameter('at', BSNumeric::getRandom());
-		}
-		return BSFlashUtility::getObjectElement($url);
-	}
-
-	/**
-	 * 出力可能か？
-	 *
-	 * @access public
-	 * @return boolean 出力可能ならTrue
-	 */
-	public function validate () {
-		return $this->isReadable() && $this->getAttributes();
-	}
-
-	/**
-	 * エラーメッセージを返す
-	 *
-	 * @access public
-	 * @return string エラーメッセージ
-	 */
-	public function getError () {
-		return 'Flashムービーではありません。';
-	}
-
-	/**
-	 * 要素が存在するか？
-	 *
-	 * @access public
-	 * @param string $key 添え字
-	 * @return boolean 要素が存在すればTrue
-	 */
-	public function offsetExists ($key) {
-		return $this->getAttributes()->hasParameter($key);
-	}
-
-	/**
-	 * 要素を返す
-	 *
-	 * @access public
-	 * @param string $key 添え字
-	 * @return mixed 要素
-	 */
-	public function offsetGet ($key) {
-		return $this->getAttribute($key);
-	}
-
-	/**
-	 * 要素を設定
-	 *
-	 * @access public
-	 * @param string $key 添え字
-	 * @param mixed 要素
-	 */
-	public function offsetSet ($key, $value) {
-		throw new BSFlashException($this . 'の属性を設定できません。');
-	}
-
-	/**
-	 * 要素を削除
-	 *
-	 * @access public
-	 * @param string $key 添え字
-	 */
-	public function offsetUnset ($key) {
-		throw new BSFlashException($this . 'の属性を削除できません。');
+	protected function getObjectElement (BSParameterHolder $params) {
+		return BSFlashUtility::getObjectElement(
+			$this->getMediaURL($params)->getContents()
+		);
 	}
 
 	/**
--- lib/carrot/media/movie/BSMovieFile.class.php
+++ lib/carrot/media/movie/BSMovieFile.class.php
@@ -10,66 +10,39 @@
  * @author 小石達也 <tkoishi@b-shock.co.jp>
  * @version $Id$
  */
-class BSMovieFile extends BSFile implements ArrayAccess {
-	private $attributes;
+class BSMovieFile extends BSMediaFile {
 	private $output;
 
 	/**
-	 * 属性を返す
+	 * ファイルを解析
 	 *
-	 * @access public
-	 * @param string $name 属性名
-	 * @return mixed 属性
+	 * @access protected
 	 */
-	public function getAttribute ($name) {
-		return $this->getAttributes()->getParameter($name);
-	}
+	protected function analize () {
+		$command = BSMovieUtility::getCommandLine();
+		$command->addValue('-i');
+		$command->addValue($this->getPath());
+		$command->addValue('2>&1', null);
+		$this->output = $command->getResult()->join("\n");
 
-	/**
-	 * 全ての属性を返す
-	 *
-	 * @access public
-	 * @return BSArray 全ての属性
-	 */
-	public function getAttributes () {
-		if (!$this->attributes) {
-			$this->attributes = new BSArray;
-			$command = BSMovieUtility::getCommandLine();
-			$command->addValue('-i');
-			$command->addValue($this->getPath());
-			$command->addValue('2>&1', null);
-			$this->output = $command->getResult()->join("\n");
-
-			if (mb_ereg('frame rate: [^\\-]+ -> ([.[:digit:]]+)', $this->output, $matches)) {
-				$this->attributes['frame_rate'] = (float)$matches[1];
-			}
-			if (mb_ereg('Duration: ([.:[:digit:]]+),', $this->output, $matches)) {
-				$this->attributes['duration'] = $matches[1];
-				$sec = BSString::explode(':', $matches[1]);
-				$this->attributes['seconds'] = ($sec[0] * 3600) + ($sec[1] * 60) + $sec[2];
-			}
-			if (mb_ereg(' ([[:digit:]]{,4}x[[:digit:]]{,4}),', $this->output, $matches)) {
-				$size = BSString::explode('x', $matches[1]);
-				$this->attributes['width'] = $size[0];
-				$this->attributes['height'] = $size[1];
-				$this->attributes['height_full'] = $size[1] + BS_MOVIE_PLAYER_HEIGHT;
-				$this->attributes['pixel_size'] = $size[0] . '×' . $size[1];
-			}
-			if (mb_ereg(' Video: ([[:alnum:]]+)', $this->output, $matches)) {
-				$this->attributes['type'] = BSMovieUtility::getType($matches[1]);
-			}
+		if (mb_ereg('frame rate: [^\\-]+ -> ([.[:digit:]]+)', $this->output, $matches)) {
+			$this->attributes['frame_rate'] = (float)$matches[1];
 		}
-		return $this->attributes;
-	}
-
-	/**
-	 * メディアタイプを返す
-	 *
-	 * @access public
-	 * @return string メディアタイプ
-	 */
-	public function getType () {
-		return $this->getAttributes()->getParameter('type');
+		if (mb_ereg('Duration: ([.:[:digit:]]+),', $this->output, $matches)) {
+			$this->attributes['duration'] = $matches[1];
+			$sec = BSString::explode(':', $matches[1]);
+			$this->attributes['seconds'] = ($sec[0] * 3600) + ($sec[1] * 60) + $sec[2];
+		}
+		if (mb_ereg(' ([[:digit:]]{,4}x[[:digit:]]{,4}),', $this->output, $matches)) {
+			$size = BSString::explode('x', $matches[1]);
+			$this->attributes['width'] = $size[0];
+			$this->attributes['height'] = $size[1];
+			$this->attributes['height_full'] = $size[1] + BS_MOVIE_PLAYER_HEIGHT;
+			$this->attributes['pixel_size'] = $size[0] . '×' . $size[1];
+		}
+		if (mb_ereg(' Video: ([[:alnum:]]+)', $this->output, $matches)) {
+			$this->attributes['type'] = BSMovieUtility::getType($matches[1]);
+		}
 	}
 
 	/**
@@ -102,49 +75,6 @@ class BSMovieFile extends BSFile implements ArrayAccess {
 	}
 
 	/**
-	 * 要素が存在するか？
-	 *
-	 * @access public
-	 * @param string $key 添え字
-	 * @return boolean 要素が存在すればTrue
-	 */
-	public function offsetExists ($key) {
-		return $this->getAttributes()->hasParameter($key);
-	}
-
-	/**
-	 * 要素を返す
-	 *
-	 * @access public
-	 * @param string $key 添え字
-	 * @return mixed 要素
-	 */
-	public function offsetGet ($key) {
-		return $this->getAttribute($key);
-	}
-
-	/**
-	 * 要素を設定
-	 *
-	 * @access public
-	 * @param string $key 添え字
-	 * @param mixed 要素
-	 */
-	public function offsetSet ($key, $value) {
-		throw new BSMovieException($this . 'の属性を設定できません。');
-	}
-
-	/**
-	 * 要素を削除
-	 *
-	 * @access public
-	 * @param string $key 添え字
-	 */
-	public function offsetUnset ($key) {
-		throw new BSMovieException($this . 'の属性を削除できません。');
-	}
-
-	/**
 	 * ムービー表示用のXHTML要素を返す
 	 *
 	 * @access public
@@ -152,80 +82,26 @@ class BSMovieFile extends BSFile implements ArrayAccess {
 	 * @return BSXMLElement 要素
 	 */
 	public function getImageElement (BSParameterHolder $params) {
-		$style = $this->getPixelSizeCSSSelector($params);
-
-		$root = new BSXMLElement('div');
-		$root->setAttribute('style', $style->getContents());
-		if (!BSString::isBlank($params['style_class'])) {
-			$root->setAttribute('class', $params['style_class']);
+		$element = parent::getImageElement($params);
+		if (($params['mode'] != 'noscript') && !$params['container_id']) {
+			$element->getElement('div')->setAttribute('style', $element->getAttribute('style'));
 		}
-
-		if ($params['mode'] == 'noscript') {
-			$root->setAttribute('style', $style->getContents());
-			$root->addElement($this->getObjectElement($params));
-		} else {
-			if (BSString::isBlank($params['container_id'])) {
-				$params['container_id'] = $this->getContainerID();
-				$container = $root->createElement('div');
-				$container->setAttribute('id', $params['container_id']);
-				$container->setAttribute('style', $style->getContents());
-			}
-			$root->addElement($this->getScriptElement($params));
-		}
-		return $root;
-	}
-
-	/**
-	 * スタイル属性を返す
-	 *
-	 * @access private
-	 * @param BSParameterHolder $params パラメータ配列
-	 * @return BSCSSSelector スタイル属性
-	 */
-	private function getPixelSizeCSSSelector (BSParameterHolder $params) {
-		$style = new BSCSSSelector;
-		if ($params['width']) {
-			$style['width'] = $params['width'] . 'px';
-		} else {
-			$style['width'] = $this['width'] . 'px';
-		}
-		if ($params['height']) {
-			$style['height'] = $params['height'] . 'px';
-		} else {
-			$style['height'] = $this['height_full'] . 'px';
-		}
-		return $style;
-	}
-
-	/**
-	 * div要素のIDを返す
-	 *
-	 * @access private
-	 * @return string div要素のID
-	 */
-	private function getContainerID () {
-		return 'flv_' . $this->getID();
+		return $element;
 	}
 
 	/**
 	 * script要素を返す
 	 *
-	 * @access private
+	 * @access protected
 	 * @param BSParameterHolder $params パラメータ配列
 	 * @return BSXMLElement 要素
 	 */
-	private function getScriptElement (BSParameterHolder $params) {
-		$url = BSURL::getInstance();
-		$url['path'] = $params['href_prefix'] . $this->getName() . $params['href_suffix'];
-		if (BSUser::getInstance()->isAdministrator()) {
-			$url->setParameter('at', BSNumeric::getRandom());
-		}
-
+	protected function getScriptElement (BSParameterHolder $params) {
 		$element = BSJavaScriptUtility::getScriptElement();
 		$body = new BSStringFormat('flowplayer(%s, %s, %s);');
 		$body[] = BSJavaScriptUtility::quote($params['container_id']);
 		$body[] = BSJavaScriptUtility::quote(BS_MOVIE_PLAYER_HREF);
-		$body[] = BSJavaScriptUtility::quote($url->getFullPath());
+		$body[] = BSJavaScriptUtility::quote($this->getMediaURL($params)->getContents());
 		$element->setBody($body->getContents());
 		return $element;
 	}
@@ -233,22 +109,16 @@ class BSMovieFile extends BSFile implements ArrayAccess {
 	/**
 	 * object要素を返す
 	 *
-	 * @access private
+	 * @access protected
 	 * @param BSParameterHolder $params パラメータ配列
 	 * @return BSXMLElement 要素
 	 */
-	private function getObjectElement (BSParameterHolder $params) {
-		$url = BSURL::getInstance();
-		$url['path'] = $params['href_prefix'] . $this->getName() . $params['href_suffix'];
-		if (BSUser::getInstance()->isAdministrator()) {
-			$url->setParameter('at', BSNumeric::getRandom());
-		}
-
+	protected function getObjectElement (BSParameterHolder $params) {
 		$element = BSFlashUtility::getObjectElement(
 			BSURL::getInstance()->setAttribute('path', BS_MOVIE_PLAYER_HREF)
 		);
 		$config = array(
-			'clip' => array('url' => $url->getContents()),
+			'clip' => array('url' => $this->getMediaURL($params)->getContents()),
 		);
 		$param = $element->createElement('param');
 		$param->setAttribute('name', 'flashvars');
