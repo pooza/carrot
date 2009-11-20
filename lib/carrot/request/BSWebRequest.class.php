fe2f2183aadc822193025a55c6a0f3be02d9270e
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2009-11-20 04:22:45 +0000

    fixes #1292
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@1628 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- lib/carrot/request/BSWebRequest.class.php
+++ lib/carrot/request/BSWebRequest.class.php
@@ -129,15 +129,15 @@ class BSWebRequest extends BSRequest {
 			$this->headers = new BSArray;
 			if (extension_loaded('http')) {
 				$headers = http_get_request_headers();
-			} else if (PHP_SAPI == 'cgi') {
+			} else if (function_exists('apache_request_headers')) {
+				$headers = apache_request_headers();
+			} else {
 				$headers = array();
 				foreach ($_SERVER as $key => $value) {
 					if (mb_ereg('HTTP_(.*)', $key, $matches)) {
 						$headers[str_replace('_', '-', $matches[1])] = $value;
 					}
 				}
-			} else {
-				$headers = apache_request_headers();
 			}
 			foreach ($headers as $key => $value) {
 				$this->setHeader($key, $value);
