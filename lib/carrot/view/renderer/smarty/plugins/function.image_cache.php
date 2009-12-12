19d1700a1f90886e1b59b1634bbe0a59ed375359
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2009-12-12 14:02:18 +0000

    refs #1260
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@1679 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- lib/carrot/view/renderer/smarty/plugins/function.image_cache.php
+++ lib/carrot/view/renderer/smarty/plugins/function.image_cache.php
@@ -16,7 +16,6 @@ function smarty_function_image_cache ($params, &$smarty) {
 	$caches = BSImageCacheHandler::getInstance();
 	$params = new BSArray($params);
 	$flags = $caches->convertFlags($params['flags']);
-
 	if (!$container = $caches->getContainer($params)) {
 		return null;
 	} else if (!$info = $container->getImageInfo($params['size'], $params['pixel'], $flags)) {
@@ -25,7 +24,6 @@ function smarty_function_image_cache ($params, &$smarty) {
 
 	$element = $caches->getImageElement($info);
 	$element->setAttribute('class', $params['style_class']);
-
 	switch ($mode = BSString::toLower($params['mode'])) {
 		case 'pixel_size':
 		case 'size':
@@ -36,13 +34,10 @@ function smarty_function_image_cache ($params, &$smarty) {
 		case 'lightbox':
 			$element = $element->wrap(new BSAnchorElement);
 			$element->setAttribute('rel', 'lightbox');
-			$url = $caches->getURL(
-				$container,
-				$params['size'],
-				$params['pixel_full'],
-				$caches->convertFlags($params['flags_full'])
+			$flags = $caches->convertFlags($params['flags_full']);
+			$element->setURL(
+				$caches->getURL($container, $params['size'], $params['pixel_full'], $flags);
 			);
-			$element->setURL($url);
 			//↓そのまま実行
 		default:
 			return $element->getContents();
