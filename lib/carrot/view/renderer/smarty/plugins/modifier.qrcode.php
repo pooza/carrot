d8d8397ab038f0c70cb72fd6bf828ff30c660db2
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2009-12-24 08:15:51 +0000

    fixes #1362
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@1714 4d49281a-7d3a-dc11-b19b-000e0cb57199

---
+++ lib/carrot/view/renderer/smarty/plugins/modifier.qrcode.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package org.carrot-framework
+ * @subpackage view.renderer.smarty.plugins
+ */
+
+/**
+ * qrcode修飾子
+ *
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ * @version $Id$
+ */
+function smarty_modifier_qrcode ($value) {
+	$url = BSURL::getInstance(null, 'BSCarrotURL');
+	$url['module'] = 'Default';
+	$url['action'] = 'QRCode';
+	$url->setParameter('value', $value);
+
+	$element = new BSImageElement;
+	$element->setURL($url);
+	return $element->getContents();
+}
+
+/* vim:set tabstop=4: */
---
+++ webapp/modules/Default/actions/QRCodeAction.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * QRCodeアクション
+ *
+ * @package org.carrot-framework
+ * @subpackage Default
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ * @version $Id$
+ */
+class QRCodeAction extends BSAction {
+	public function execute () {
+		$qrcode = new BSQRCode;
+		$qrcode->setData($this->request['value']);
+		$this->request->setAttribute('renderer', $qrcode);
+		return BSView::SUCCESS;
+	}
+}
+
+/* vim:set tabstop=4: */
