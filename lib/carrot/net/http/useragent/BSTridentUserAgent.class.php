f5773eefb953830a5bc2350706b522c60c0ff7ba
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2010-01-09 10:31:51 +0000

    refs #1379
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@1750 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- lib/carrot/net/http/useragent/BSTridentUserAgent.class.php
+++ lib/carrot/net/http/useragent/BSTridentUserAgent.class.php
@@ -21,7 +21,7 @@ class BSTridentUserAgent extends BSUserAgent {
 	public function __construct ($name = null) {
 		parent::__construct($name);
 		$this->bugs['cache_control'] = true;
-		$this->attributes['is_kuso'] = $this->isLegacy();
+		$this->attributes['is_kuso'] = ($this->getVersion() < 8);
 		$this->attributes['is_ie' . floor($this->getVersion())] = true;
 	}
 
