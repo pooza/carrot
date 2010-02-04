ff5542063658f4c84e429c99307fb0e101b6d7f4
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2010-02-04 10:55:53 +0000

    refs #1424
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@1816 4d49281a-7d3a-dc11-b19b-000e0cb57199

---
+++ lib/carrot/geocode/BSGeocodeEntry.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package org.carrot-framework
+ * @subpackage geocode
+ */
+
+/**
+ * ジオコード エントリーレコード
+ *
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ * @version $Id$
+ */
+class BSGeocodeEntry extends BSRecord {
+}
+
+/* vim:set tabstop=4: */
