f48b99ec8b39d59da9c13fc199b82b48af1b2ac9
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2010-02-05 02:05:43 +0000

    fixes #1427
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@1823 4d49281a-7d3a-dc11-b19b-000e0cb57199

---
+++ lib/carrot/geocode/BSGeocodeException.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package org.carrot-framework
+ * @subpackage geocode
+ */
+
+/**
+ * ジオコード例外
+ *
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ * @version $Id$
+ */
+class BSGeocodeException extends BSException {
+}
+
+/* vim:set tabstop=4: */
---
+++ lib/carrot/view/smarttag/BSMapTag.class.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * @package org.carrot-framework
+ * @subpackage view.smarttag
+ */
+
+/**
+ * Google Mapタグ
+ *
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ * @version $Id$
+ */
+class BSMapTag extends BSSmartTag {
+	private $geocode;
+
+	/**
+	 * タグ名を返す
+	 *
+	 * @access public
+	 * @return string タグ名
+	 */
+	public function getTagName () {
+		return 'map';
+	}
+
+	/**
+	 * 置換して返す
+	 *
+	 * @access public
+	 * @param string $body 置換対象文字列
+	 * @return string 置換された文字列
+	 * @abstract
+	 */
+	public function execute ($body) {
+		try {
+			if ($this->getUserAgent()->isMobile()) {
+				$element = $this->getMobileAnchorElement();
+			} else {
+				$element = $this->getAjaxDivisionElement();
+			}
+			$replace = $element->getContents();
+		} catch (Exception $e) {
+			$replace = sprintf('[エラー: %s]', $e->getMessage());
+		}
+		return str_replace($this->getContents(), $replace, $body);
+	}
+
+	private function getMobileAnchorElement () {
+		$url = BSURL::getInstance();
+		$url['host'] = 'www.google.co.jp';
+		$url['path'] = '/m/local';
+		$url->setParameter('q', $this->tag[1]);
+		$element = new BSAnchorElement;
+		$element->setBody($this->tag[1]);
+		$element->setURL($url);
+		return $element;
+	}
+
+	private function getAjaxDivisionElement () {
+		$params = $this->getQueryParameters();
+		$geocode = $this->getGeocode();
+
+		$element = new BSDivisionElement;
+		$inner = $element->addElement(new BSDivisionElement);
+		$script = $element->addElement(new BSScriptElement);
+
+		$inner->setID('map_' . BSCrypt::getSHA1($this->tag[1] . BS_CRYPT_SALT));
+		$inner->setStyle('width', $params['width']);
+		$inner->setStyle('height', $params['height']);
+		$inner->setBody('Loading...');
+
+		$statement = 'actions.onload.push(function(){handleGoogleMaps($(%s), %f, %f);});';
+		$statement = new BSStringFormat($statement);
+		$statement[] = BSJavaScriptUtility::quote($inner->getID());
+		$statement[] = $geocode['lat'];
+		$statement[] = $geocode['lng'];
+		$script->setBody($statement->getContents());
+
+		return $element;
+	}
+
+	private function getGeocode () {
+		if (!$this->geocode) {
+			$service = new BSGoogleMapsService;
+			if (!$this->geocode = $service->getGeocode($this->tag[1])) {
+				throw new BSGeocodeException('ジオコードが取得できません。');
+			}
+		}
+		return $this->geocode;
+	}
+}
+
+/* vim:set tabstop=4: */
