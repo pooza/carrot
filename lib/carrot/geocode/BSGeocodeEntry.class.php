d58f92acfd2cc6b17ce882777241ab0fb77abf5a
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2010-06-23 14:54:46 +0000

    fixes #1790
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@2169 4d49281a-7d3a-dc11-b19b-000e0cb57199

--- lib/carrot/geocode/BSGeocodeEntry.class.php
+++ lib/carrot/geocode/BSGeocodeEntry.class.php
@@ -11,6 +11,7 @@
  * @version $Id$
  */
 class BSGeocodeEntry extends BSRecord {
+	private $stations;
 
 	/**
 	 * 書式化して返す
@@ -21,6 +22,26 @@ class BSGeocodeEntry extends BSRecord {
 	 */
 	public function format ($separator = ',') {
 		return $this['lat'] . $separator . $this['lng'];
+	}
+
+	/**
+	 * 最寄り駅を返す
+	 *
+	 * @access public
+	 * @param integer $flags フラグのビット列
+	 *   BSHeartRailsExpressService::FORCE_QUERY 新規取得を強制
+	 * @return BSArray 最寄り駅
+	 */
+	public function getStations ($flags = null) {
+		if (!$this->stations) {
+			$this->stations = new BSArray;
+			try {
+				$service = new BSHeartRailsExpressService;
+				$this->stations->setParameters($service->getStations($this, $flags));
+			} catch (Exception $e) {
+			}
+		}
+		return $this->stations;
 	}
 }
 
