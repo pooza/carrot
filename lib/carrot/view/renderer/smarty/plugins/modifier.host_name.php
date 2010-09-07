725d98e6e20ad13f5e0e21400c2071b7afb00280
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2010-09-07 09:39:42 +0000

    fixes #2032
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@2334 4d49281a-7d3a-dc11-b19b-000e0cb57199

---
+++ lib/carrot/view/renderer/smarty/plugins/modifier.host_name.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package org.carrot-framework
+ * @subpackage view.renderer.smarty.plugins
+ */
+
+/**
+ * URLからホスト名を抽出する修飾子
+ *
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ * @version $Id$
+ */
+function smarty_modifier_host_name ($value) {
+	if (is_array($value)) {
+		return $value;
+	} else if ($value instanceof BSParameterHolder) {
+		return $value->getParameters();
+	} else if (!BSString::isBlank($value)
+		&& ($url = BSURL::getInstance($value))
+		&& ($url instanceof BSHTTPURL)) {
+
+		return $url['host']->getName();
+	}
+}
+/* vim:set tabstop=4: */
