40d38f879f3a44696358e089859ca3b3050def61
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2010-09-29 11:31:45 +0000

    fixes #2074
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@2367 4d49281a-7d3a-dc11-b19b-000e0cb57199

---
+++ tests/carrot/service/twitter/BSTwitterSearchServiceTest.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package org.carrot-framework
+ */
+
+/**
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ * @version $Id$
+ * @abstract
+ */
+class BSTwitterSearchServiceTest extends BSTest {
+
+	/**
+	 * 実行
+	 *
+	 * @access public
+	 */
+	public function execute () {
+		$this->assert('__construct', $service = new BSTwitterSearchService);
+		$this->assert('searchTweets_twitter', !!$service->searchTweets('twitter')->count());
+		$this->assert('searchTweets_#twitter', !!$service->searchTweets('#twitter')->count());
+	}
+}
+
+/* vim:set tabstop=4: */
