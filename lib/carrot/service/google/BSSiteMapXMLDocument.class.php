f46f5bec04e441dc2ca9a5b7d3dee9bf840ff7db
pooza <pooza@4d49281a-7d3a-dc11-b19b-000e0cb57199>
2011-01-24 12:49:27 +0000

    fixes #2205
    git-svn-id: http://svn.carrot-framework.org/carrot/trunk@2468 4d49281a-7d3a-dc11-b19b-000e0cb57199

---
+++ lib/carrot/service/google/BSSiteMapXMLDocument.class.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package org.carrot-framework
+ * @subpackage service.google
+ */
+
+/**
+ * Google sitemap.xml サイトマップ文書
+ *
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ * @version $Id$
+ * @link http://www.google.com/support/webmasters/bin/answer.py?hl=ja&answer=183668
+ */
+class BSSiteMapXMLDocument extends BSXMLDocument {
+
+	/**
+	 * @access public
+	 * @param string $name 要素の名前
+	 */
+	public function __construct ($name = null) {
+		parent::__construct('urlset');
+		$this->setNamespace('http://www.sitemaps.org/schemas/sitemap/0.9');
+	}
+
+	/**
+	 * サイトマップ情報を返す
+	 *
+	 * @access public
+	 * @param BSHTTPRedirector $url 対象URL
+	 * $param BSParameterHolder $params パラメータ配列
+	 */
+	public function register (BSHTTPRedirector $url, BSParameterHolder $params = null) {
+		$element = new BSXMLElement('url');
+		$element->createElement('loc', $url->getURL()->getContents());
+		if ($params) {
+			foreach ($params as $key => $value) {
+				if (!BSString::isBlank($value)) {
+					if ($key == 'lastmod') {
+						$date = BSDate::getInstance($value);
+						$value = $date->format(DateTime::W3C);
+					}
+					$element->createElement($key, $value);
+				}
+			}
+		}
+		$this->addElement($element);
+	}
+}
+
+/* vim:set tabstop=4: */
