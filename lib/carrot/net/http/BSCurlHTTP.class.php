879fcd74ff2783f13b6aa3d85d4d06cc1b701a6e
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2011-05-04 10:53:47 +0900

    fixes #2391

--- lib/carrot/net/http/BSCurlHTTP.class.php
+++ lib/carrot/net/http/BSCurlHTTP.class.php
@@ -121,10 +121,8 @@ class BSCurlHTTP extends BSHTTP {
 		$url['path'] = '/' . ltrim($href, '/');
 		if ($this->isSSL()) {
 			$url['scheme'] = 'https';
-			$url['port'] = BSNetworkService::getPort('https');
 		} else {
 			$url['scheme'] = 'http';
-			$url['port'] = BSNetworkService::getPort('http');
 		}
 		return $url;
 	}
