7bd1e33455cadc825fb503d438a0b9c01e6210d1
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2011-05-19 22:50:31 +0900

    fixes #2433

--- lib/carrot/filter/BSLegacyUserAgentFilter.class.php
+++ lib/carrot/filter/BSLegacyUserAgentFilter.class.php
@@ -9,26 +9,8 @@
  *
  * @author 小石達也 <tkoishi@b-shock.co.jp>
  */
-class BSDeniedUserAgentFilter extends BSFilter {
+class BSLegacyUserAgentFilter extends BSFilter {
 	public function initialize ($params = array()) {
-		if (BS_DEBUG || $this->user->isAdministrator()) {
-			if (!BSString::isBlank($name = $this[BSUserAgent::ACCESSOR])) {
-				$this->request->setUserAgent(BSUserAgent::create($name));
-			}
-		}
-
-		$names = new BSArray(array(
-			BSTridentUserAgent::ACCESSOR => BSTridentUserAgent::DEFAULT_NAME,
-			BSWebKitUserAgent::ACCESSOR => BSWebKitUserAgent::DEFAULT_NAME,
-		));
-		foreach ($names as $field => $name) {
-			if ($this->request[$field] || $this->user->getAttribute($field)) {
-				$this->user->setAttribute($field, 1);
-				$this->request->setUserAgent(BSUserAgent::create($name));
-				break;
-			}
-		}
-
 		$this['module'] = 'Default';
 		$this['action'] = 'DeniedUserAgent';
 		return parent::initialize($params);
