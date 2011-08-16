c90d4f4f46ef0b4cc49eef60412b9881bace9da0
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2011-08-16 11:14:53 +0900

    fixes #2618

--- tests/carrot/net/http/useragent/BSGeckoUserAgentTest.class.php
+++ tests/carrot/net/http/useragent/BSGeckoUserAgentTest.class.php
@@ -9,7 +9,7 @@ class BSGeckoUserAgentTest extends BSTest {
 		$useragent = BSUserAgent::create(
 			'Mozilla/5.0 (X11; U; Linux i686; rv:1.7.3) Gecko/20040913 Firefox/0.10'
 		);
-		$this->assert('getInstance_Fx010', $useragent instanceof BSGeckoUserAgent);
+		$this->assert('create_Fx010', $useragent instanceof BSGeckoUserAgent);
 		$this->assert('getVersion_Fx010', $useragent->getVersion() == '1.7.3');
 		$this->assert('isLegacy_Fx010', $useragent->isLegacy());
 
@@ -17,7 +17,7 @@ class BSGeckoUserAgentTest extends BSTest {
 		$useragent = BSUserAgent::create(
 			'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9) Gecko/2008051206 Firefox/3.0'
 		);
-		$this->assert('getInstance_Fx3', $useragent instanceof BSGeckoUserAgent);
+		$this->assert('create_Fx3', $useragent instanceof BSGeckoUserAgent);
 		$this->assert('getVersion_Fx3', $useragent->getVersion() == '1.9');
 		$this->assert('isLegacy_Fx3', !$useragent->isLegacy());
 		$this->assert('hasSupport_flash_Fx3', $useragent->hasSupport('flash'));
