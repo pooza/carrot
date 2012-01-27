ba53bdc7ab6052e2edd91012ed29a2c6b2f045ee
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2012-01-27 16:24:59 +0900

    以前削除したJSファイルをobsoletedとして追加。

---
+++ share/obsoleted/ahah.js
@@ -0,0 +1,29 @@
+/**
+ * AHAHエンジン
+ *
+ * @package org.carrot-framework
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ */
+
+function ahah (id, href) {
+  if (window.XMLHttpRequest) {
+    var req = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+    var req = new ActiveXObject('Microsoft.XMLHTTP');
+  } else {
+    return false;
+  }
+
+  var element = document.getElementById(id);
+  req.onreadystatechange = function() {
+    if (req.readyState < 4) {
+      return;
+    } else if (req.status == 200) {
+      element.innerHTML = req.responseText;
+    } else {
+      element.innerHTML = 'AHAH Error: ' + req.statusText;
+    }
+  }
+  req.open('GET', href, true);
+  req.send('');
+}
