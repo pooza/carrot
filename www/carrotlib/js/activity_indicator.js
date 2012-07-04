99410e30db12b3d2219a4de118b9f58f5e0bbd41
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2012-07-04 16:55:57 +0900

    fixes #8964

--- www/carrotlib/js/activity_indicator.js
+++ www/carrotlib/js/activity_indicator.js
@@ -43,8 +43,15 @@ ActivityIndicator = Class.create({
 });
 
 document.observe('dom:loaded', function () {
-  var indicator = new ActivityIndicator();
+  // IE8未満は対象外
+  var matched;
+  if (matched = navigator.userAgent.match(/MSIE ([1-7])/)) {
+    if (matched[1] < 8) {
+      return;
+    }
+  }
 
+  var indicator = new ActivityIndicator();
   $$('form').each(function (frm) {
     frm.observe('submit', function (event) {
       indicator.show();
