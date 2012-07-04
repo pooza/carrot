6d2722edfa282cc0897b3097f495967b9fd3d959
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2012-07-04 18:11:28 +0900

    refs #8967

--- tests/carrot/xml/xhtml/BSXHTMLElementTest.class.php
+++ tests/carrot/xml/xhtml/BSXHTMLElementTest.class.php
@@ -5,12 +5,15 @@
  */
 class BSXHTMLElementTest extends BSTest {
 	public function execute () {
+		$element = new BSXHTMLElement('input');
+		$element->setEmptyElement(true);
+		$element->setAttribute('type', 'checkbox');
+		$element->setAttribute('checked', 'checked');
+
 		if (BS_VIEW_HTML5) {
-			$element = new BSXHTMLElement('input');
-			$element->setEmptyElement(true);
-			$element->setAttribute('type', 'checkbox');
-			$element->setAttribute('checked', 'checked');
 			$this->assert('getContents', $element->getContents() == '<input type="checkbox" checked>');
+		} else {
+			$this->assert('getContents', $element->getContents() == '<input type="checkbox" checked="checked" />');
 		}
 	}
 }
