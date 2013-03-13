eba55dd09e645e5cbcd71d7adff6dfa1c3505756
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2013-03-13 17:37:28 +0900

    fixes #9024

--- lib/carrot/filter/BSHostSecurityFilter.class.php
+++ lib/carrot/filter/BSHostSecurityFilter.class.php
@@ -10,6 +10,8 @@
  * @author 小石達也 <tkoishi@b-shock.co.jp>
  */
 class BSHostSecurityFilter extends BSFilter {
+	private $networks;
+
 	public function execute () {
 		try {
 			$this->auth();
@@ -20,12 +22,27 @@ class BSHostSecurityFilter extends BSFilter {
 	}
 
 	private function auth () {
-		foreach (BSAdministratorRole::getInstance()->getAllowedNetworks() as $network) {
+		foreach ($this->getNetworks() as $network) {
 			if ($network->isContain($this->request->getHost())) {
 				return true;
 			}
 		}
 		throw new BSNetException('リモートアクセス禁止のホストです。');
+	}
+
+	private function getNetworks () {
+		if (!$this->networks) {
+			$this->networks = new BSArray;
+			if (BSString::isBlank(BS_ADMIN_NETWORKS)) {
+				$this->networks[] = new BSNetwork('0.0.0.0/0');
+			} else {
+				$this->networks[] = new BSNetwork('127.0.0.1/32');
+				foreach (BSString::explode(',', BS_ADMIN_NETWORKS) as $network) {
+					$this->networks[] = new BSNetwork($network);
+				}
+			}
+		}
+		return $this->networks;
 	}
 }
 
