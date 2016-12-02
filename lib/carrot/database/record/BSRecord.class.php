a602506ffd8a3981477cfea172958c81bdeb53c7
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2016-12-02 16:34:46 +0900

    refs #151

--- lib/carrot/database/record/BSRecord.class.php
+++ lib/carrot/database/record/BSRecord.class.php
@@ -10,10 +10,9 @@
  * @author 小石達也 <tkoishi@b-shock.co.jp>
  * @abstract
  */
-abstract class BSRecord implements ArrayAccess, BSSerializable, BSAssignable,
-	BSImageContainer, BSHTTPRedirector, BSAttachmentContainer {
+abstract class BSRecord implements ArrayAccess,
+	BSSerializable, BSAssignable, BSAttachmentContainer, BSImageContainer, BSHTTPRedirector {
 
-	use BSAttachmentContainerMethods;
 	use BSHTTPRedirectorMethods;
 	protected $attributes;
 	protected $table;
@@ -319,6 +318,26 @@ abstract class BSRecord implements ArrayAccess, BSSerializable, BSAssignable,
 		}
 	}
 
+	/**
+	 * 添付ファイルの情報を返す
+	 *
+	 * @access public
+	 * @param string $name 名前
+	 * @return string[] 添付ファイルの情報
+	 */
+	public function getAttachmentInfo ($name = null) {
+		if ($file = $this->getAttachment($name)) {
+			$info = new BSArray;
+			$info['path'] = $file->getPath();
+			$info['size'] = $file->getSize();
+			$info['type'] = $file->getType();
+			$info['filename'] = $this->getAttachmentFileName($name);
+			if ($url = $this->getAttachmentURL($name)) {
+				$info['url'] = $url->getContents();
+			}
+			return $info;
+		}
+	}
 
 	/**
 	 * 添付ファイルを返す
@@ -357,6 +376,40 @@ abstract class BSRecord implements ArrayAccess, BSSerializable, BSAssignable,
 		}
 		$file->rename($this->getAttachmentBaseName($name) . $suffix);
 		$file->moveTo($this->getTable()->getDirectory());
+	}
+
+	/**
+	 * 添付ファイルベース名を返す
+	 *
+	 * @access public
+	 * @param string $name 名前
+	 * @return string 添付ファイルベース名
+	 */
+	public function getAttachmentBaseName ($name) {
+		return sprintf('%06d_%s', $this->getID(), $name);
+	}
+
+	/**
+	 * 添付ファイルのダウンロード時の名を返す
+	 *
+	 * @access public
+	 * @param string $name 名前
+	 * @return string ダウンロード時ファイル名
+	 */
+	public function getAttachmentFileName ($name = null) {
+		if ($file = $this->getAttachment($name)) {
+			return $this->getAttachmentBaseName($name) . $file->getSuffix();
+		}
+	}
+
+	/**
+	 * 添付ファイルのURLを返す
+	 *
+	 * @access public
+	 * @param string $name 名前
+	 * @return BSURL 添付ファイルURL
+	 */
+	public function getAttachmentURL ($name = null) {
 	}
 
 	/**
