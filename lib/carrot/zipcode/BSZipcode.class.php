aff143ff5dd7bdc7b9b38be646da6403c43afb00
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2016-12-17 18:45:06 +0900

    fixes #186

--- lib/carrot/zipcode/BSZipcode.class.php
+++ lib/carrot/zipcode/BSZipcode.class.php
@@ -13,9 +13,6 @@ class BSZipcode implements BSAssignable {
 	private $contents;
 	private $major;
 	private $minor;
-	private $file;
-	private $info;
-	private $pref;
 	const PATTERN = '^([[:digit:]]{3})-?([[:digit:]]{4})$';
 
 	/**
@@ -41,65 +38,6 @@ class BSZipcode implements BSAssignable {
 			$this->contents = sprintf('%s-%s', $this->major, $this->minor);
 		}
 		return $this->contents;
-	}
-
-	private function getInfo () {
-		if (!$this->info) {
-			$service = new BSAjaxZip3Service;
-			$addresses = $service->getAddresses($this->major);
-			if ($info = $addresses[$this->major. $this->minor]) {
-				$this->info = new BSArray($info);
-			}
-		}
-		return $this->info;
-	}
-
-	/**
-	 * 都道府県を返す
-	 *
-	 * @access public
-	 * @return string 都道府県
-	 */
-	public function getPref () {
-		if (!$this->pref && $this->getInfo()) {
-			$config = BSConfigManager::getInstance()->compile('postal');
-			$this->pref = $config['prefs'][$this->getInfo()->getParameter(0) - 1];
-		}
-		return $this->pref;
-	}
-
-	/**
-	 * 市区町村を返す
-	 *
-	 * @access public
-	 * @return string 市区町村
-	 */
-	public function getCity () {
-		if ($this->getInfo()) {
-			return $this->getInfo()->getParameter(1);
-		}
-	}
-
-	/**
-	 * 町域を返す
-	 *
-	 * @access public
-	 * @return string 町域
-	 */
-	public function getLocality () {
-		if ($this->getInfo()) {
-			return $this->getInfo()->getParameter(2);
-		}
-	}
-
-	/**
-	 * 住所を返す
-	 *
-	 * @access public
-	 * @return string 住所
-	 */
-	public function getAddress () {
-		return $this->getPref() . $this->getCity() . $this->getLocality();
 	}
 
 	/**
